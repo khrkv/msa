e0ea9cf11f18c9a7c28929e10065babf614880e9
Egor Kharkov <kharkov_ed@outlook.com>
2022-11-24 00:09:32 +0300

    Done

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "YC_TOKEN" {
+  type        = string
+  description = "Yandex Cloud OAuth token"
+}
+
+variable "YC_CLOUD" {
+  type        = string
+  description = "Yandex Cloud ID"
+}
+
+variable "YC_FOLDER" {
+  type        = string
+  description = "Yandex Cloud folder"
+}
